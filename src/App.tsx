78dcc419de7c247221197552b4522453a57043de
Barbara <barbara.taskinen@gmail.com>
2023-09-19 17:59:27 +0300

    Set initial layout of page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,21 @@ const App = () => {
     <>
       <div>
         <h1>Is There A Game?</h1>
+        <div className="App_mainContent">
+          <img
+            className="App_ilvesLogo"
+            src="https://upload.wikimedia.org/wikipedia/fi/5/52/Tampereen_Ilveksen_logo.svg"
+            alt="Logo of Tampere Ilves Ice Hockey Team"
+          />
+          <button className="App_findOutButton" type="button">
+            Click to Find out!
+          </button>
+          <img
+            className="App_tapparaLogo"
+            src="https://upload.wikimedia.org/wikipedia/fi/3/32/Tapparan_logo.svg"
+            alt="Logo of Tampere Tappera Ice Hockey Team"
+          />
+        </div>
       </div>
     </>
   );
